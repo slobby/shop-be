e122f04542e428cd8452e93e2892dcc1d6ec29be
Andrew <andslobby@gmail.com>
2021-09-08 17:47:50 +0300

    fix: add validation to Product.title

--- product-service/src/interfaces/CreateProductSchema.ts
+++ product-service/src/interfaces/CreateProductSchema.ts
@@ -1,11 +1,11 @@
 export default {
   type: 'object',
   properties: {
-    title: { type: 'string' },
+    title: { type: 'string', minLength: 1 },
     description: { type: 'string' },
     count: { type: 'integer', minimum: 0 },
     price: { type: 'number', minimum: 0, exclusiveMinimum: true },
-    image: { type: 'string' },
+    image: { type: 'string', minLength: 1 },
   },
   required: ['title', 'description', 'count', 'price', 'image'],
   additionalProperties: false,
--- product-service/src/interfaces/productSchema.ts
+++ product-service/src/interfaces/productSchema.ts
@@ -2,11 +2,11 @@ export default {
   type: 'object',
   properties: {
     id: { type: 'string', format: 'uuid' },
-    title: { type: 'string' },
+    title: { type: 'string', minLength: 1 },
     description: { type: 'string' },
     count: { type: 'integer', minimum: 0 },
     price: { type: 'number', minimum: 0, exclusiveMinimum: 0 },
-    image: { type: 'string' },
+    image: { type: 'string', minLength: 1 },
   },
   required: ['id', 'title', 'description', 'count', 'price', 'image'],
   additionalProperties: false,
