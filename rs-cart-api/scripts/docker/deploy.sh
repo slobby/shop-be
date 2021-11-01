0c5d3a7279772ac29ddb6ff6a41e452db4e26672
Andrew <andslobby@gmail.com>
2021-11-01 17:40:27 +0300

    task9: add cart-service

---
+++ rs-cart-api/scripts/docker/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ENV=development
+ENV=develop
+
+REGION=eu-west-1
+AWS_ACCOUNT_ID=892398644316
+PROFILE=default
+
+AWS_DOCKER_REGISTRY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+
+docker logout $AWS_DOCKER_REGISTRY_URL
+# Login in AWS container registry with your AWS credentials
+aws ecr get-login-password --profile $PROFILE --region $REGION | docker login --username AWS --password-stdin $AWS_DOCKER_REGISTRY_URL
+
+# Deploy app
+eb deploy $ENV
